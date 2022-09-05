12118775bf32022f70184d15d41df5d04c5a7a9b
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-09-05 21:26:18 +0300

    Initial commit

---
+++ Task-0/Program.cs
@@ -0,0 +1,42 @@
+﻿/*----------------------------------------------------------------------------------------------------------------------
+Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+----------------------------------------------------------------------------------------------------------------------*/
+
+int[] CreateRandomArray(int size, int minVal, int maxVal)
+{
+    int[] newArr=new int[size];
+
+    for(int i=0; i<size; i++)
+        newArr[i]= new Random().Next(minVal,maxVal+1);
+
+    return newArr;
+}
+
+void ShowArr(int[] arr)
+{
+
+    Console.Write("[");
+    for(int i=0; i<arr.Length;i++)
+        if(i==arr.Length-1)
+            Console.Write($"{arr[i]} ]");
+        else
+            Console.Write($"{arr[i]}, ");
+    Console.WriteLine();
+}
+
+
+int[] ReverseArray(int[] array)
+{
+    for(int i = 0, j = array.Length -1 ; i<j; i++, j--)
+    {
+        int temp = array[i];
+        array[i] = array[j];       
+        array[j] = temp;
+    }
+    return array;
+}
+
+int[] newArray = CreateRandomArray(8, 10, 99);
+
+ShowArr(newArray);
+ShowArr(ReverseArray(newArray));
---
+++ Task-2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*----------------------------------------------------------------------------------------------------------------------
+Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: a и b.
+----------------------------------------------------------------------------------------------------------------------*/
+void ShowArr(int[] arr)
+{
+    Console.Write("[");
+    for(int i=0; i<arr.Length;i++)
+        if(i==arr.Length-1)
+            Console.Write($"{arr[i]} ]");
+        else
+            Console.Write($"{arr[i]}, ");
+    Console.WriteLine();
+}
+
+int[] GenFib(int a, int b, int n)
+{
+    int[] x = new int[n];
+    x[0] = a;
+    x[1] = b;
+    for(int i=2; i<n; i++)
+        x[i] = x[i-2] + x[i-1];
+    return x;
+}
+
+Console.Write("Введите первое число ряда: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ряда: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество чисел ряда: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ShowArr(GenFib(a,b,n));
+
